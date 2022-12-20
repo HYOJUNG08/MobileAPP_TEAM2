035501f930145703948e65172be66e0fce071ec6
alicehjjung <alicehjjung@gmail.com>
2022-12-20 18:16:52 +0900

    Add and read XML file data2

--- app/src/main/java/com/example/tproject/Fc2main.kt
+++ app/src/main/java/com/example/tproject/Fc2main.kt
@@ -2,12 +2,10 @@ package com.example.tproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import javax.xml.parsers.DocumentBuilderFactory
 
 class Fc2main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fc2main)
-
     }
 }
